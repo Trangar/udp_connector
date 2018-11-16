baa4b4e29cdc24a02fab5dc6dd126870ec8ccd2b
Victor Koenders <victor.koenders@gmail.com>
2018-11-16 09:25:26 +0100

    Added docs and cleaned up code

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,33 @@
+#![deny(missing_docs)]
+//! A generic eventually-correct framework over unreliable connections, such as UDP.
+//!
+//! This crate uses a struct called `Connector` that eventually guarantees that all persistent messages
+//! arrive at the other end, or it will disconnect.
+//!
+//! This crate differentiates between two message types:
+//! * Confirmed: This is a message that is:
+//! ** Guaranteed to arrive ***at some point***
+//! ** Not guaranteed to arrive in the correct order
+//! * Unconfirmed: This is a message that is not guaranteed to arrive
+//!
+//! Use cases can be:
+//! * Sending player data does not always have to arrive, because the location is updated 10 times a second (unconfirmed)
+//! * Login information should always arrive, but this can take a second (confirmed)
+
 #[macro_use]
 extern crate serde_derive;
 
-mod client;
 mod packet;
 mod param;
-mod server;
 
 #[cfg(test)]
 pub mod test;
 
+/// The result that is used in this type. It is a simple wrapper around `Result<T, failure::Error>`
 pub type Result<T> = std::result::Result<T, failure::Error>;
 
-pub use self::client::ClientConnector;
-pub use self::packet::Packet;
-pub use self::param::{BoundImpl, ConnectorParam};
-pub use self::server::ServerConnector;
+use self::packet::Packet;
+pub use self::param::ConnectorParam;
 
 use std::collections::HashMap;
 use std::io::ErrorKind;
@@ -67,6 +80,13 @@ impl Default for ConnectorReceive {
     }
 }
 
+/// The connector is used to handle handshakes and timeouts with a different, remote connector
+///
+/// For client-side applications, we recommend calling `update_and_receive` at a frequent rate
+///
+/// For server-side applications, we recommend dealing with your own UdpSocket receiving logic, looking up the connector based on a SocketAddr, and then calling `handle_incoming_data`.
+///
+/// The connector struct has a lot of config settings. All these settings can be found in `ConnectorParam`
 pub struct Connector<TParam: ConnectorParam> {
     /// Contains data about the sending half of this connector
     send: ConnectorSend<TParam>,
@@ -92,10 +112,16 @@ struct CachedPacket<TSend> {
     pub last_emit_time: f64,
 }
 
+/// The state of the connector. This is based on when the last ping was send or received. Changing your ConnectorParam will greatly affect the results of `Connector.state()`, returning this value.
 #[derive(Debug, Eq, PartialEq)]
 pub enum NetworkState {
+    /// We received a ping a reasonable amount of time ago, so we're connected. See `ConnectorParam::PING_INTERVAL_S` for more info.
     Connected,
+
+    /// We have not received a ping for a while, and we are not connecting at this point in time. See `ConnectorParam::RECEIVE_PING_TIMEOUT_S` for more info.
     Disconnected,
+
+    /// We have not received a ping for a while but we did try to connect. See `ConnectorParam::SEND_PING_TIMEOUT_S` for more info.
     Connecting,
 }
 
@@ -108,9 +134,15 @@ impl MissingId {
     }
 }
 
+/// A generic trait over a socket. This is automatically implemented for `UdpSocket` but can be implemented for your own connector as well.
 pub trait Socket {
+    /// Receive data from any remote, returning the amount of bytes read, and the SocketAddr that the data was received from
     fn recv_from(&mut self, buffer: &mut [u8]) -> std::io::Result<(usize, SocketAddr)>;
+
+    /// The local SocketAddr we're listening on
     fn local_addr(&self) -> SocketAddr;
+
+    /// Send data to the given SocketAddr
     fn send_to(&mut self, buffer: &[u8], target: SocketAddr) -> Result<()>;
 }
 
@@ -128,6 +160,7 @@ impl Socket for UdpSocket {
 }
 
 impl<TParam: ConnectorParam> Connector<TParam> {
+    /// Create a Connector that is bound to the given remote SocketAddr
     pub fn bound_to(peer_addr: SocketAddr) -> Self {
         Connector {
             send: Default::default(),
@@ -135,14 +168,23 @@ impl<TParam: ConnectorParam> Connector<TParam> {
             peer_addr,
         }
     }
+
+    /// Get the socket address that this connector is paired with
     pub fn bound_addr(&self) -> SocketAddr {
         self.peer_addr
     }
+
+    /// Connect to the `bound_addr`. This will reset the internal state of the connector, and start up the connection handshake
     pub fn connect(&mut self, socket: &mut Socket) -> Result<()> {
         self.send = Default::default();
         self.receive = Default::default();
         self.send_ping(socket)
     }
+
+    /// Get the current state of this connector. This is dependent on a couple of settings in ConnectorParam:
+    /// * If we have received a ping since `ConnectorParam::RECEIVE_PING_TIMEOUT_S` ago, we're connected
+    /// * If we have send a ping since `ConnectorParam::SEND_PING_TIMEOUT_S` ago, we're connecting
+    /// * Else we're disconnected
     pub fn state(&self) -> NetworkState {
         if self.receive.last_ping_time + TParam::RECEIVE_PING_TIMEOUT_S < time::precise_time_s() {
             if self.send.last_ping_time + TParam::SEND_PING_TIMEOUT_S > time::precise_time_s() {
@@ -155,6 +197,9 @@ impl<TParam: ConnectorParam> Connector<TParam> {
         }
     }
 
+    /// Receive data from the other connector. This will call `handle_incoming_data` internally.
+    ///
+    /// Ideally you would never need this function. Use `update_and_receive` on clients, and `handle_incoming_data` on servers.
     pub fn receive_from(&mut self, socket: &mut Socket) -> Result<Vec<TParam::TReceive>> {
         let mut buffer = [0u8; 1024];
         let mut result = Vec::new();
@@ -181,16 +226,18 @@ impl<TParam: ConnectorParam> Connector<TParam> {
         }
     }
 
+    /// Update this connector and receive data from the remote connector.
     pub fn update_and_receive(&mut self, socket: &mut Socket) -> Result<Vec<TParam::TReceive>> {
         self.update(socket)?;
         self.receive_from(socket)
     }
 
+    /// Update this connector. This will make sure the connection is still intact and requests any potentially missing packets.
     pub fn update(&mut self, socket: &mut Socket) -> Result<()> {
         if NetworkState::Disconnected == self.state() {
             return Ok(());
         }
-        if self.send.last_ping_time + TParam::CONNECTION_TIMEOUT_S < time::precise_time_s() {
+        if self.send.last_ping_time + TParam::PING_INTERVAL_S < time::precise_time_s() {
             self.send_ping(socket)?;
         }
         for missing_packet in &mut self.receive.missing_message_id_list {
@@ -218,6 +265,10 @@ impl<TParam: ConnectorParam> Connector<TParam> {
         Ok(())
     }
 
+    /// Handles incoming data. This will perform internal logic to make sure data is being transmitted correctly,
+    /// and requests missing packets.
+    ///
+    /// Any actual data that was received, will be returned from this function.
     pub fn handle_incoming_data(
         &mut self,
         socket: &mut Socket,
@@ -225,6 +276,7 @@ impl<TParam: ConnectorParam> Connector<TParam> {
     ) -> Result<Option<TParam::TReceive>> {
         let packet: Packet<_> = bincode::deserialize(data)?;
         println!("Received {:?}", packet);
+        // TODO: This should probably be simplified. Currently it's just a big match statement
         Ok(match packet {
             Packet::Ping {
                 last_send_message_id,
@@ -324,6 +376,9 @@ impl<TParam: ConnectorParam> Connector<TParam> {
         self.receive.last_message_id = NonZeroU64::new(id);
     }
 
+    /// Send an unconfirmed message to the other connector. It is not guaranteed that this message will ever arrive.
+    ///
+    /// This is useful for data that does not have to arrive. Think of things like player movements, frames of a lossy video stream, etc.
     pub fn send_unconfirmed(&mut self, socket: &mut Socket, msg: TParam::TSend) -> Result<()> {
         send_packet_to(
             self.peer_addr,
@@ -336,6 +391,7 @@ impl<TParam: ConnectorParam> Connector<TParam> {
         Ok(())
     }
 
+    /// Send a confirmed message to the other connector. The connector will try to make sure this message arrives. It is not guaranteed that messages will arrive in the same order at the other side.
     pub fn send_confirmed(&mut self, socket: &mut Socket, msg: TParam::TSend) -> Result<()> {
         let sending_id = if let Some(id) = self.send.next_message_id {
             id
@@ -359,7 +415,7 @@ impl<TParam: ConnectorParam> Connector<TParam> {
     }
 }
 
-pub fn send_packet_to<TSend: serde::Serialize>(
+fn send_packet_to<TSend: serde::Serialize>(
     peer_addr: SocketAddr,
     socket: &mut Socket,
     packet: &Packet<TSend>,
--- src/param.rs
+++ src/param.rs
@@ -1,18 +1,73 @@
 use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 
+/// Settings that are set up for a Connector. This can be used to tweak your Connector at compile-time
 pub trait ConnectorParam {
+    /// The type that this connector will be sending. This is usually an enum.
+    ///
+    /// ```rust
+    /// # #[macro_use]
+    /// # extern crate serde_derive;
+    /// # extern crate serde;
+    /// # use udp_connector::ConnectorParam;
+    ///
+    /// // For the server:
+    /// #[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+    /// pub enum ServerToClient {
+    ///     LoginResult { success: bool },
+    /// }
+    ///
+    /// struct ConnectorConfig;
+    ///
+    ///
+    /// impl ConnectorParam for ConnectorConfig {
+    ///     type TSend = ServerToClient;
+    ///     // Other fields omitted
+    ///     # type TReceive = ServerToClient;
+    /// }
+    /// # fn main() {}
+    /// ```
     type TSend: for<'a> Deserialize<'a> + Serialize + Debug + Eq + PartialEq;
+
+    /// The type that this connector will be sending. This is usually an enum.
+    ///
+    /// ```rust
+    /// # #[macro_use]
+    /// # extern crate serde_derive;
+    /// # extern crate serde;
+    /// # use udp_connector::ConnectorParam;
+    ///
+    /// # type AuthenticateParams = u32;
+    ///
+    /// // For the server:
+    /// #[derive(Debug, Serialize, Deserialize, Eq, PartialEq)]
+    /// pub enum ClientToServer {
+    ///     Login { params: AuthenticateParams },
+    /// }
+    ///
+    /// struct ConnectorConfig;
+    ///
+    /// impl ConnectorParam for ConnectorConfig {
+    ///     type TReceive = ClientToServer;
+    ///     // Other fields omitted
+    ///     # type TSend = ClientToServer;
+    /// }
+    /// # fn main() {}
+    /// ```
     type TReceive: for<'a> Deserialize<'a> + Serialize + Debug + Eq + PartialEq;
 
-    const CONNECTION_TIMEOUT_S: f64 = 1.;
+    /// The interval at which pings are being emitted to the other connector. This should be set in relation to `RECEIVE_PING_TIMEOUT_S` and `SEND_PING_TIMEOUT_S`, and how often you expect to lose packets.
+    const PING_INTERVAL_S: f64 = 0.5;
+
+    /// The interval at which missing packets are being requested from the connector
     const REQUEST_MISSING_PACKET_INTERVAL_S: f64 = 1.;
+
+    /// The interval at which unconfirmed packets are being send to the other connector
     const EMIT_UNCONFIRMED_PACKET_INTERVAL_S: f64 = 1.;
 
-    const RECEIVE_PING_TIMEOUT_S: f64 = Self::CONNECTION_TIMEOUT_S * 2.;
-    const SEND_PING_TIMEOUT_S: f64 = Self::CONNECTION_TIMEOUT_S * 2.;
-}
+    /// The time that it takes before this connector assumes it has lost connection to the other connector
+    const RECEIVE_PING_TIMEOUT_S: f64 = Self::PING_INTERVAL_S * 3.;
 
-pub trait BoundImpl {
-    fn ping_received(&mut self);
+    /// The time that it takes before this connector assumes it has lost connection to the other connector
+    const SEND_PING_TIMEOUT_S: f64 = Self::PING_INTERVAL_S * 3.;
 }
